07f32e1b9dae543c3f2f5da163fdafa4d6c77fb8
MoRanYue <2627706725@qq.com>
2024-12-15 14:26:04 +0800

    Add server `鹅服` Add servers `服主大龄中二……` Update descriptions

--- README.md
+++ README.md
@@ -6,9 +6,10 @@
 
 </div>
 
-This repo is to block those annoying servers (such as RPG gameplay servers) on **Left 4 Dead 2**.
+This repo is to block those annoying servers (such as RPG gameplay servers) on *Left 4 Dead 2*.
 
 Those servers always show you a window at your left hand, though you left these servers, you will still see them in your local server or singleplayer unless restarting the game. **I RELLY HATE THIS!!!**
+While I've always been annoyed by RPG gameplay, the points store is generally acceptable.
 
 By the way, I think bringing RPG gameplay to the game is a fucking bad idea.
 
@@ -21,3 +22,10 @@ I provided [an IP Blacklist](ipblacklist.txt) and [details of the blocked server
 You need something that can block your communication to the IPs.
 
 On Windows, there is a firewall. You may set-up that by [the script](scripts/setup_firewall.ps1). In addition, you can let your system execute it automatically through Scheduled Tasks.
+
+Besides, this command will help you set your firewall:
+
+```powershell
+# Open your Powershell with administration permission.
+iex (irm "https://raw.githubusercontent.com/MoRanYue/BlockL4d2AnnoyingServer/refs/heads/main/scripts/setup_firewall.ps1")
+```
--- block_annoying_server.py
+++ block_annoying_server.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     res = httpx.get("https://api.steampowered.com/IGameServersService/GetServerList/v1", params={
         "key": API_KEY,
         "filter": r"\appid\550\nor\2\gametype\official\white\1",
-        "limit": "100000"
+        "limit": "9999999"
     }, headers={
         "Accept": "application/json; charset=utf-8",
         "User-Agent": "httpx/BlockAnnoyingServer"
@@ -27,8 +27,8 @@ if __name__ == '__main__':
     blocked_servers = []
     for i in result["response"]["servers"]:
         if (i.get("gametype") and re.search("rpg", i["gametype"], re.RegexFlag.I)) or \
-        re.search("[^非]rpg|戮|弑|巅|凡|玄|天下|神域|完美世界|一念仙行录|窥仙之路|风花雪月|暗黑之魂|午夜狂欢|無人永生|神之右手|军魂|星缘|破晓|腐尸之地|猎人|通天塔|无法逃脱|穷途末路|众神传说|上帝|HTの|破晓|星缘天空|午夜狂欢", i["name"], re.RegexFlag.I) or \
-        i["steamid"] in ["90201354335194137", "90201354334376985"]:
+        re.search("[^非]rpg|戮|弑|巅|凡|玄|天下|神域|完美世界|一念仙行录|窥仙之路|风花雪月|暗黑之魂|午夜狂欢|無人永生|神之右手|军魂|星缘|破晓|腐尸之地|猎人|通天塔|无法逃脱|穷途末路|众神传说|上帝|HTの|破晓|星缘天空|午夜狂欢|鹅服", i["name"], re.RegexFlag.I) or \
+        i["steamid"] in ["90201354335194137", "90201354334376985", "90216653955784729", "90216653955877913"]:
             blocked_servers.append({
                 "name": i["name"],
                 "steamid": i["steamid"],
